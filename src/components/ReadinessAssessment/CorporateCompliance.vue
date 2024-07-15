49f708f3ba84765a917acb343ad9c56fc313181e
Chukwuma Obasi <ccobasi8@gmail.com>
2024-07-15 09:14:51 +0100

    update corporate compliance component

--- src/components/ReadinessAssessment/CorporateCompliance.vue
+++ src/components/ReadinessAssessment/CorporateCompliance.vue
@@ -3,9 +3,171 @@
     <div class="title">
       <h3>Corporate and Compliance</h3>
     </div>
-
+    <div class="content">
+      <div class="row">
+        <div class="col-6">
+          <label for="">1. Summary of the history of the Business from incorporation to date</label>
+          <div class="radio">
+            <input type="radio" id="Yes1" name="question1" value="Yes">
+            <label for="Yes1">Yes</label>
+            <input type="radio" id="No1" name="question1" value="No">
+            <label for="No1">No</label>
+          </div>
+        </div>
+        <div class="col-6">
+          <label for="">2. Copy of Certificate of Incorporation, Memorandum and Articles of Association, most recent CAC7 (Directors) and CAC2 (Shareholding)</label>
+          <div class="radio">
+            <input type="radio" id="Yes2" name="question2" value="Yes">
+            <label for="Yes2">Yes</label>
+            <input type="radio" id="No2" name="question2" value="No">
+            <label for="No2">No</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <label for="">3. Copy of Incorporation Documents of any SPV in relation to the project (if applicable)</label>
+          <div class="radio">
+            <input type="radio" id="Yes1" name="question1" value="Yes">
+            <label for="Yes1">Yes</label>
+            <input type="radio" id="No1" name="question1" value="No">
+            <label for="No1">No</label>
+          </div>
+        </div>
+        <div class="col-6">
+          <label for="">4. Profiles of all shareholders holding at least 5.0% interest in the Company</label>
+          <div class="radio">
+            <input type="radio" id="Yes2" name="question2" value="Yes">
+            <label for="Yes2">Yes</label>
+            <input type="radio" id="No2" name="question2" value="No">
+            <label for="No2">No</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <label for="">5. Details of any subsidiary or associated companies of the Company together with details of the relationship and the Company’s shareholding in such entitiesUntitled</label>
+          <div class="radio">
+            <input type="radio" id="Yes1" name="question1" value="Yes">
+            <label for="Yes1">Yes</label>
+            <input type="radio" id="No1" name="question1" value="No">
+            <label for="No1">No</label>
+          </div>
+        </div>
+        <div class="col-6">
+          <label for="">6. Details and copies of all share subscription agreements, shareholder agreements, voting agreements and other relevant agreements governing the relationship of the Company's shareholders that is material to the Business or Assets</label>
+          <div class="radio">
+            <input type="radio" id="Yes2" name="question2" value="Yes">
+            <label for="Yes2">Yes</label>
+            <input type="radio" id="No2" name="question2" value="No">
+            <label for="No2">No</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <label for="">7. Profile of Directors, Senior Management and Company Secretary (e.g. work experience, schools attended, degrees obtained, other qualifications)</label>
+          <div class="radio">
+            <input type="radio" id="Yes1" name="question1" value="Yes">
+            <label for="Yes1">Yes</label>
+            <input type="radio" id="No1" name="question1" value="No">
+            <label for="No1">No</label>
+          </div>
+        </div>
+        <div class="col-6">
+          <label for="">8. Organisation Chart that includes: Staff Strength of the Business</label>
+          <div class="radio">
+            <input type="radio" id="Yes2" name="question2" value="Yes">
+            <label for="Yes2">Yes</label>
+            <input type="radio" id="No2" name="question2" value="No">
+            <label for="No2">No</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <label for="">9. Organisation Chart that includes: Business Permit and Expatriate Quota (where the Company has foreign (non-Nigerian) shareholding)</label>
+          <div class="radio">
+            <input type="radio" id="Yes1" name="question1" value="Yes">
+            <label for="Yes1">Yes</label>
+            <input type="radio" id="No1" name="question1" value="No">
+            <label for="No1">No</label>
+          </div>
+        </div>
+        <div class="col-6">
+          <label for="">10. Promoter And Technical Team’s Profile and competencies</label>
+          <div class="radio">
+            <input type="radio" id="Yes2" name="question2" value="Yes">
+            <label for="Yes2">Yes</label>
+            <input type="radio" id="No2" name="question2" value="No">
+            <label for="No2">No</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <label for="">11. Details of all business location</label>
+          <div class="radio">
+            <input type="radio" id="Yes1" name="question1" value="Yes">
+            <label for="Yes1">Yes</label>
+            <input type="radio" id="No1" name="question1" value="No">
+            <label for="No1">No</label>
+          </div>
+        </div>
+        <div class="col-6">
+          <label for="">12. Summary of lines of businessUntitled</label>
+          <div class="radio">
+            <input type="radio" id="Yes2" name="question2" value="Yes">
+            <label for="Yes2">Yes</label>
+            <input type="radio" id="No2" name="question2" value="No">
+            <label for="No2">No</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <label for="">13. Details and status of key projects (completed, on-going, expected completion dates etc.)</label>
+          <div class="radio">
+            <input type="radio" id="Yes1" name="question1" value="Yes">
+            <label for="Yes1">Yes</label>
+            <input type="radio" id="No1" name="question1" value="No">
+            <label for="No1">No</label>
+          </div>
+        </div>
+        <div class="col-6">
+          <label for="">14. Company policies on operations and maintenance</label>
+          <div class="radio">
+            <input type="radio" id="Yes2" name="question2" value="Yes">
+            <label for="Yes2">Yes</label>
+            <input type="radio" id="No2" name="question2" value="No">
+            <label for="No2">No</label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <label for="">15. Company policies on procurement and supply chain</label>
+          <div class="radio">
+            <input type="radio" id="Yes1" name="question1" value="Yes">
+            <label for="Yes1">Yes</label>
+            <input type="radio" id="No1" name="question1" value="No">
+            <label for="No1">No</label>
+          </div>
+        </div>
+        <div class="col-6">
+          <label for="">16. Details of internal controls, invoicing/​credit policies, operational procedures, internal audit and compliance function</label>
+          <div class="radio">
+            <input type="radio" id="Yes2" name="question2" value="Yes">
+            <label for="Yes2">Yes</label>
+            <input type="radio" id="No2" name="question2" value="No">
+            <label for="No2">No</label>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
 <style scoped>
 body,
 html {
@@ -29,12 +191,37 @@ html {
   font-weight: 600;
   color: #000;
 }
-.origination p {
+.content {
+  max-width: 100%;
+  gap: 30px;
+  display: flex;
+  flex-direction: column;
+  opacity: 1;
+}
+.row {
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+}
+.col-6 {
+  flex: 1;
+  padding: 10px;
+}
+.col-6 label {
+  display: block;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 19.2px;
+  text-align: left;
+}
+.radio {
+  display: flex;
+  align-items: center;
+  gap: 10px;
   margin-top: 10px;
 }
-.diagram img {
-  max-width: 100%;
-  height: auto;
-  margin-top: 20px;
+.radio input {
+  margin-right: 5px;
 }
 </style>
